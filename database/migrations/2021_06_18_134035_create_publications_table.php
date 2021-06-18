f5ef347aed2e9fa89ba3f50b8ae4ecd89991e74c
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-06-18 14:04:32 -0500

    #1 Publication y migración

---
+++ database/migrations/2021_06_18_134035_create_publications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->string('career', 200);
+            $table->text('description');
+            $table->float('hours',3,2);
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications');
+    }
+}
