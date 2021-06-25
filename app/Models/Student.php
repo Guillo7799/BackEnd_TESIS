bc963340feaa610b65ed260caa22745050a5e418
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-06-24 20:25:43 -0500

    Creación de modelo student y business

---
+++ app/Models/Student.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Student as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Student extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'identification',
+        'name',
+        'last_name',
+        'email',
+        'birthday',
+        'university',
+        'career',
+        'province',
+        'location',
+        'language',
+        'level',
+        'habilities',
+        'certificaties',
+        'second_degree',
+        'laboral_experience',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
