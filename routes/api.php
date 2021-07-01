c9839799e0c208cc3adb523c67d8b31de0520d35
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-07-01 12:36:25 -0500

    #8 Controlador de Article Cambio en las rutas

--- routes/api.php
+++ routes/api.php
@@ -15,21 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('publications', function () {
-    return Publication::all();
-});
-Route::get('publications/{id}', function ($id) {
-    return Publication::find($id);
-});
-Route::post('publications', function (Request $request) {
-    return Publication::create($request->all());
-});
-Route::put('publications/{id}', function (Request $request, $id) {
-    $Publication = Publication::findOrFail($id);
-    $Publication->update($request->all());
-    return $Publication;
-});
-Route::delete('publications/{id}', function ($id) {
-    Publication::find($id)->delete();
-    return 204;
-});
+//Rutas para Publication
+
+Route::get('publications', 'App\Http\Controllers\PublicationController@index');
+Route::get('publications/{publication}', 'App\Http\Controllers\PublicationController@show');
+Route::post('publications', 'App\Http\Controllers\PublicationController@store');
+Route::put('publications/{publication}', 'App\Http\Controllers\PublicationController@update');
+Route::delete('publications/{publication}', 'App\Http\Controllers\PublicationController@delete');
